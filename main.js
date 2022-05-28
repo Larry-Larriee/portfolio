73057515dfe07a94141d84dfdf8b04ffa2889951
Larry Le <lle114@student.hbuhsd.edu>
2022-05-28 11:51:47 -0700

    Minor Unimportant Changes

---
+++ main.js
@@ -0,0 +1,17 @@
+// When the user clicks a key, it sends the key to the server and runs the function below
+document.onkeydown = function(key) {
+
+    // key property (char name)
+    if (key.key == "Enter") {
+        window.alert("Secret Message: Hello :D");
+    }
+}
+
+var counter = 0;
+
+function incrementOne(){
+    counter++;
+    document.getElementById("timeElapse").innerHTML = "Time Passed: " + counter;
+}
+
+//setInterval(incrementOne, 1000);
