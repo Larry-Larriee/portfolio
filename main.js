6bbdda60d73ee17e0cec6ada91f029839ed7f36f
Larry Le <lle114@student.hbuhsd.edu>
2022-05-30 21:06:19 -0700

    Minor Changes

--- main.js
+++ main.js
@@ -7,41 +7,6 @@ document.onkeydown = function(key) {
     }
 }
 
-function setupCookie(cName, cValue, exDay){
-    // Const variables cannot be reassigned
-    const d = new Date();
+Cookies.set("username", "John");
 
-    // set the time to expire in x days (starts in miliseconds)
-    d.setTime(d.getTime() + (exDay*24*60*60*1000)); 
-
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = cName + "=" + cValue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-  
-  function checkCookie() {
-    let user = getCookie("username");
-    if (user != "") {
-      alert("Welcome again " + user);
-    } else {
-       user = prompt("Please enter your name:","");
-       if (user != "" && user != null) {
-         setCookie("username", user, 30);
-       }
-    }
-  }
+console.log(Cookies.get("username"));
